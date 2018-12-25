0dd4597b2e7102fccdd869c5ff366c66e65f1678
Jackowski7 <Dale.jackowski@gmail.com>
2018-12-25 11:49:19 -0500

    Initial commit

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	CharacterController character;
+
+	public float speed = 1;
+
+	public float verticalDeadzone = .1f;
+	public float horizontalDeadzone = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > verticalDeadzone)
+		{
+			character.SimpleMove(Vector3.forward * speed);
+		}
+		if (Input.GetAxis("Vertical") < -verticalDeadzone)
+		{
+			character.SimpleMove(-Vector3.forward * speed);
+		}
+		if (Input.GetAxis("Horizontal") > horizontalDeadzone)
+		{
+			character.SimpleMove(Vector3.right * speed);
+		}
+		if (Input.GetAxis("Horizontal") < -horizontalDeadzone)
+		{
+			character.SimpleMove(-Vector3.right * speed);
+		}
+	}
+}
---
+++ Assets/_Project/Scripts/WorldGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+
+	public GameObject player;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+
+
+
+}
+
+/*
+ Get player
+ for ungenrerated rows in front of player closer than x distance, generate rows of nodes.
+
+	*/
